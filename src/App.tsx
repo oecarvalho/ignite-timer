6855962f49e918ca55e1344af96ef8e973471a3e
Felipe <felipe.carvalho.dm@gmail.com>
2025-08-20 17:53:19 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import './App.css'
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export default function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
       <GlobalStyle/>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
