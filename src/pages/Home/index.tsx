f8e8fb3d416d81a659b56608d04da2d5bbf13896
Felipe <felipe.carvalho.dm@gmail.com>
2025-08-22 00:25:03 -0300

    finalizando home, iniciando history

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Play } from "phosphor-react";
+import { CountdownCointainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+
+
+export function Home(){
+    return (
+        <HomeContainer>
+            <form action=''>
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput type="text" id="task" placeholder="Dê um nome para o seu projeto" list="task-suggestions"/>
+
+                    <datalist id='task-suggestions'>
+                        <option value="Banana"></option>
+                        <option value="Banana"></option>
+                        <option value="Banana"></option>
+                        <option value="Banana"></option>
+                        <option value="Banana"></option>
+                    </datalist>
+
+                    <label htmlFor="">durante</label>
+                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" step={5} min={5} max={60}/>
+
+                    <span>minutos.</span>
+                </FormContainer>
+            
+            
+                <CountdownCointainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownCointainer>
+
+                <StartCountdownButton type="submit" disabled>
+                    <Play size={24}/>
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
